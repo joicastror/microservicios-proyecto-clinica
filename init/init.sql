74e98f6292190b6becd53098a737f6f41eb8c735
Ignacio Castro <neitwys@gmail.com>
2026-05-20 17:31:23 -0400

    Se cambio el formato y se normalizó los nombres y url de las bases de datos

---
+++ init/init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS clinica_autenticacion_db;
+CREATE DATABASE IF NOT EXISTS clinica_citas_db;
+CREATE DATABASE IF NOT EXISTS clinica_fichaclinica_db;
+CREATE DATABASE IF NOT EXISTS clinica_pagos_db;
+CREATE DATABASE IF NOT EXISTS clinica_examenes_db;
+CREATE DATABASE IF NOT EXISTS clinica_recetas_db;
+CREATE DATABASE IF NOT EXISTS clinica_notificaciones_db;
+CREATE DATABASE IF NOT EXISTS clinica_soporte_db;

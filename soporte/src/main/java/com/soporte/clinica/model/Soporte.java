f2a8dbf5351352c700fc7c8adc3a767b6c1b5029
Ignacio Castro <neitwys@gmail.com>
2026-05-20 17:51:57 -0400

    Se normalizó los microservicios con su URL para el Docker

--- soporte/src/main/java/com/soporte/clinica/model/Soporte.java
+++ soporte/src/main/java/com/soporte/clinica/model/Soporte.java
@@ -7,9 +7,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "soporte")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Soporte {
 
     private Long id;

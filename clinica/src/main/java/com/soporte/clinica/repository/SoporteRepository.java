21e9d5f774e1065469afa5f0e8f758c416f01d9a
Matias <mat.figueroav@duocuc.cl>
2026-05-19 20:43:33 -0400

    Agregado microservicio clinica_soporte

---
+++ clinica/src/main/java/com/soporte/clinica/repository/SoporteRepository.java
@@ -0,0 +1,7 @@
+package com.soporte.clinica.repository;
+
+import com.soporte.clinica.model.Soporte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoporteRepository extends JpaRepository<Soporte, Long> {
+}
